4c16a88abf89b2fbfd8fadff088f76dc221a3a1d
Justice Okpala <justicecharles497@gmail.com>
2020-10-26 18:27:01 +0100

    first commit

---
+++ task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] a = new int[5] { 10, 20, 30, 40, 50 };
+
+            int[] b = new int[5] { 5, 4, 3, 2, 1 };
+
+            int[] arr = new int[5];
+
+            for (int i = 0, j = 0; i < a.Length; i++, j++)
+
+            {
+
+                arr[i] = a[i] * b[j];                             
+
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+
+            {
+
+                Console.Write(arr[i] + " ");
+
+            }
+
+            Console.Read();
+
+        }
+    }
+}
